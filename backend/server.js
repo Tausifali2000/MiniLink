82066d0880fd22fd57d400c7988e336d312c7953
TausifAli2000 <tausifali2000@gmail.com>
2025-02-13 12:30:15 +0530

    deploye changes

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://mern-app-btfqarbmg0bfceh8.southeastasia-01.azurewebsites.net", 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true 
 }));
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
 		proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				target: "https://mern-app-btfqarbmg0bfceh8.southeastasia-01.azurewebsites.net",
 				
 			},
 
